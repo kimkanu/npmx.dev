51699f6f9a93334146d060de8ef21085fce2ab49
Keonwoo Kim <main@kanu.kim>
2026-06-07 00:03:18 +0900

    fix(i18n): fix reported problems by ci runs

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,5 +1,21 @@
 import { currentLocales, datetimeFormats, numberFormats, pluralRules } from '../config/i18n'
 
+/**
+ * Get the correct Korean particle following the given word.
+ *
+ * Korean usually has a pair of two particles, which is chosen depending on
+ * whether a word ends with a consonant or a vowel.
+ * This function deals with such Korean particles, with a plausible heuristic
+ * for non-Korean characters.
+ *
+ * @param text
+ * @param isRo whether the particle pair is 으로/로 or not
+ *        If isRo is true, `withFinalConsonant` excludes ㄹ (L sound) from
+ *        its consonant set.
+ * @param withFinalConsonant a version in case ending with a consonant
+ * @param withoutFinalConsonant a version in case ending with a vowel
+ * @returns the correct particle
+ */
 function getKoreanJosa<T>(
   text: T,
   isRo: boolean,
@@ -8,8 +24,9 @@ function getKoreanJosa<T>(
 ) {
   if (typeof text !== 'string') return text
 
-  // Strip out all non-Korean non-alphanumeric characters.
-  const normalizedText = text.replace(/[^ㄱ-ㅎㅏ-ㅣ가-힣a-z0-9]/gi, '')
+  // Strip out all non-Korean (vowels, consonants, and complete characters)
+  // non-alphanumeric characters.
+  const normalizedText = text.replace(/[^\u3131-\u3163\uac00-\ud7a3a-z0-9]/gi, '')
 
   if (typeof normalizedText !== 'string' || normalizedText.length === 0)
     return withoutFinalConsonant
@@ -24,7 +41,7 @@ function getKoreanJosa<T>(
 
   // If `text` ends with complete Korean characters,
   // check if it has a syllable final sound and return the appropriate one.
-  if (/[가-힣]$/.test(normalizedText)) {
+  if (/[\uac00-\ud7a3]$/.test(normalizedText)) {
     const code = normalizedText.charCodeAt(normalizedText.length - 1) - 0xac00
     const hasFinalConsonant = !(code % 28 === 0 || (isRo && code % 28 === 8))
     return hasFinalConsonant ? withFinalConsonant : withoutFinalConsonant
@@ -32,8 +49,10 @@ function getKoreanJosa<T>(
 
   // The case `text` ends with incomplete Korean characters (consonants or vowels)
   if (isRo && normalizedText.endsWith('ㄹ')) return withoutFinalConsonant
-  if (/[ㄱ-ㅎ]$/.test(normalizedText)) return withFinalConsonant
-  if (/[ㅏ-ㅣ]$/.test(normalizedText)) return withoutFinalConsonant
+  // Consonant cases
+  if (/[\u3131-\u314e]$/.test(normalizedText)) return withFinalConsonant
+  // Vowel cases
+  if (/[\u314f-\u3163]$/.test(normalizedText)) return withoutFinalConsonant
 
   // The case `text` ends with a single latin alphabet or a digit
   const singleAlphabetOrNumericRegex = isRo ? /[mn036]$/i : /[lmnr013678]$/i
